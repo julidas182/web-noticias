73a05be4616148d65b195ffcf1a0d3cf25be0708
julidas <juliandascenzo@gmail.com>
2022-08-23 00:21:23 -0300

    repo1

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("hola");
+
+document.querySelector('.menu-btn').addEventListener('click', ()=> {
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500});
+ScrollReveal().reveal('.cards-banner-uno', { delay: 500});
+ScrollReveal().reveal('.cards-banner-dos', { delay: 500});
